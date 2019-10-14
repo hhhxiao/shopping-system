7692147847a31864a9324f36bdb7fedfb13e306b
hhhxiao <1417566491@qq.com>
2019-10-14 13:17:28 +0800

    Initial commit

---
+++ src/main/java/com/xhy/shoppingsystem/ShoppingSystemApplication.java
@@ -0,0 +1,19 @@
+package com.xhy.shoppingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ShoppingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingSystemApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
---
+++ src/test/java/com/xhy/shoppingsystem/ShoppingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xhy.shoppingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoppingSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
