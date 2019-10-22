246fef036b76688c81495b8330e95ed5febcb883
hhhxiao <1417566491@qq.com>
2019-10-22 18:26:16 +0800

    写完index.html的前端接口

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,52 @@
+function gene_card_html(item) {
+    return `<div class="card">
+<div class="image">
+                <img src="../images/steve.jpg" alt="商品图片">
+                </div>
+                    <div class="content">
+                        <h5 class="ui red header">${item.price}元</h5>
+                        <h5 class="ui header"><a href="/item/${item.itemId}">${item.itemName}</a></h5>
+                        ${item.manufact}</div>
+                        </div>`;
+}
+
 $(document).ready(function () {
-    alert("我被加载了@@@");
+    $.ajax({
+        url: "/test/get-item",
+        type: "get",
+        contentType: "application/json",
+        success:
+            function (data) {
+                data.forEach(function (item) {
+                    $('#item-context').append(gene_card_html(item));
+                });
+            }
+    });
+
+
+    $('#search').bind('keypress', function (event) {
+        if (event.keyCode == "13") {
+            var name = $('#search').val();
+            console.log(name);
+            $.ajax({
+                url: "/test/search",
+                type: "post",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    'itemName': name,
+                }),
+                success:
+                    function (data) {
+                        let html = "";
+                        data.forEach(function (item) {
+                            html += gene_card_html(item);
+                        });
+                        $('#item-context').html(html);
+                    }
+            });
+        }
+    });
+
     $(".ui.dropdown").dropdown();
 });
