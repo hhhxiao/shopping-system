511d8fb74fe80994e859ede0d5c1b1070bad67c0
hhhxiao <1417566491@qq.com>
2019-10-24 21:47:59 +0800

    添加mapper文件

--- src/test/java/com/xhy/shoppingsystem/ShoppingSystemApplicationTests.java
+++ src/test/java/com/xhy/shoppingsystem/ShoppingSystemApplicationTests.java
@@ -1,7 +1,10 @@
 package com.xhy.shoppingsystem;
 
+import com.xhy.shoppingsystem.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ShoppingSystemApplicationTests {
 
+
+    @Autowired
+    UserService userService;
+
     @Test
     public void contextLoads() {
+
     }
 
 }
